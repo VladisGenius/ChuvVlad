e5a52e98d547dd0c3eb7fe1b63c000a41ed5c58f
Vladislove <vladislove05@mail.ru>
2023-10-21 10:54:03 +0500

    first commit

---
+++ task1_21.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num, mx, mn;
+	cout << "Enter your number:";
+	cin >> num;
+	if (num / 10 > num % 10)
+	{
+		mn = num % 10; mx = num / 10;
+	}
+	else
+	{
+		mx = num % 10; mn = num / 10;
+	}
+	cout << "Max number:" << mx << endl;
+	cout << "Min number:" << mn << endl;
+}
---
+++ task2_21.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x, y;
+	cout << "Enter x y:" << endl;
+	cin >> x >> y;
+	if (x > 0) {
+		if (y > 0)
+			cout << "1st quarter";
+		else
+			cout << "4rd quarter";
+	}
+	if (x < 0) {
+		if (y > 0)
+			cout << "2nd quarter";
+		else
+			cout << "3rd quarter";
+
+	}
+}
